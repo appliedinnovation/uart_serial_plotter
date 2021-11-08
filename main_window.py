7c8712688b90de4598097a8211f23720099731e9
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-11-07 19:46:18 -0600

    Fixed minor bugs in update_plot, now handling error cases (missing headers etc.)

--- main_window.py
+++ main_window.py
@@ -423,7 +423,6 @@ class MainWindow(QMainWindow):
             if group_dict["col"] != "":
 
                 ANSI_FOREGROUND_COLOR_MAP = {
-                    # lowercase b
                     "\x1b[0;30m": "#000000",  # Black
                     "\x1b[0;31m": "#FF073A",  # Red
                     "\x1b[0;32m": "#1FFF0F",  # Green
@@ -432,15 +431,6 @@ class MainWindow(QMainWindow):
                     "\x1b[0;35m": "#EA00FF",  # Magenta
                     "\x1b[0;36m": "#00FFFF",  # Cyan
                     "\x1b[0;37m": "#FBFFFF",  # White
-                    # uppercase B
-                    "\x1B[0;30m": "#000000",  # Black
-                    "\x1B[0;31m": "#FF073A",  # Red
-                    "\x1B[0;32m": "#1FFF0F",  # Green
-                    "\x1B[0;33m": "#FFC42E",  # Yellow
-                    "\x1B[0;34m": "#4D4DFF",  # Blue
-                    "\x1B[0;35m": "#EA00FF",  # Magenta
-                    "\x1B[0;36m": "#00FFFF",  # Cyan
-                    "\x1B[0;37m": "#FBFFFF",  # White
                 }
 
                 # Check if ANSI color code is specified using:
@@ -506,7 +496,6 @@ class MainWindow(QMainWindow):
 
         # Strip all ANSI style characters
         ANSI_STYLES = {
-            # lowercase b
             "\x1b[m",
             "\x1b[0m",  # Reset
             "\x1b[1m",  # Bold
@@ -515,15 +504,6 @@ class MainWindow(QMainWindow):
             "\x1b[4m",  # Underlined
             "\x1b[7m",  # Inverse
             "\x1b[9m",  # Strikethrough
-            # uppercase B
-            "\x1B[m",
-            "\x1B[0m",  # Reset
-            "\x1B[1m",  # Bold
-            "\x1B[2m",  # Faint
-            "\x1B[3m",  # Italic
-            "\x1B[4m",  # Underlined
-            "\x1B[7m",  # Inverse
-            "\x1B[9m",  # Strikethrough
         }
 
         for style in ANSI_STYLES:
@@ -537,10 +517,10 @@ class MainWindow(QMainWindow):
         self.output_editor.setTextCursor(cursor)
         self.output_editor.ensureCursorVisible()
 
-        arrdata = strdata.split(",")
+        arrdata = escaped_strdata.strip().split(",")
 
         # return if there was not a comma
-        if len(arrdata) < 5:
+        if len(arrdata) < 2:
             return
 
         # determine if this line is a header or not (first value is string data)
@@ -556,17 +536,29 @@ class MainWindow(QMainWindow):
             self.plot_page.plot.set_header(arrdata)
         else:
             # an array of numbers
-            datapoint = [float(x.strip()) for x in arrdata]
+            try:
+                datapoint = [float(x.strip()) for x in arrdata]
 
-            if len(self.plot_page.plot.trace_names) == 0:
-                # Header not set
-                # Maybe we didn't receive it over UART
-                # Set the Header to be: "Time","Signal_1", "Signal_2",...
-                header = ["Time"]
-                header.extend(["Signal_" + str(i) for i in range(len(datapoint))])
-                self.plot_page.plot.set_header(header)
-
-            self.plot_page.plot.update_data([datapoint])
+                if len(self.plot_page.plot.trace_names) == 0 or len(
+                    self.plot_page.plot.trace_names
+                ) < len(datapoint):
+                    # Header not set
+                    # Maybe we didn't receive it over UART
+                    # Set the Header to be: "Time","Signal_1", "Signal_2",...
+                    header = ["Time"]
+                    header.extend(
+                        ["Signal_" + str(i) for i in range(len(datapoint) - 1)]
+                    )
+                    self.plot_page.plot.set_header(header)
+                    self.plot_page.plot.update_data([datapoint])
+                elif len(self.plot_page.plot.trace_names) == len(datapoint):
+                    self.plot_page.plot.update_data([datapoint])
+                else:
+                    self.log_info(
+                        "Not a valid datapoint: '{}'", escaped_strdata.strip()
+                    )
+            except:
+                pass
 
     def __on_usb_device_arrival__(self):
         self.log_info("Detected New USB Device")
