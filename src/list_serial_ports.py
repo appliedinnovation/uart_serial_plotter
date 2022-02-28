b959f145a67359f00c3f513fa6f086ab0399938e
William Emfinger <waemfinger@gmail.com>
2022-02-28 09:22:10 -0600

    update for serial report descriptor from the Atari programmer board.

--- src/list_serial_ports.py
+++ src/list_serial_ports.py
@@ -9,12 +9,14 @@ def list_serial_ports():
     :returns:
         A list of the serial ports available on the system
     """
-    port_descriptors = ["USB Serial Port", "TTL232R-3V3", "USB to UART", "usbserial", "RS232"]
+    port_descriptors = ["USB Serial Port", "TTL232R-3V3", "USB to UART", "usbserial", "RS232", "USB-UART"]
     ports = list(serial.tools.list_ports.comports())
     result = []
+    # print("Found {} ports:".format(len(ports)))
     for p in ports:
         is_correct_type_of_port = False
         for desc in port_descriptors:
+            # print("\t{}".format(p.description))
             if desc in p.description:
                 is_correct_type_of_port = True
         if is_correct_type_of_port:
